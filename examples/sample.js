2148b813d762fc863449c757213d6da749eb4bbf
Xavier Platinum <kwislawrencekwis@gmail.com>
2022-08-11 05:23:30 +0100

    added samples

--- examples/sample.js
+++ examples/sample.js
@@ -1,6 +1,5 @@
 var Required = require('../');
 console.log(Required.all());
-// console.log(Required.states());
-// console.log(Required.senatorialDistricts("Plateau"));
-// console.log(Required.lgas("oyo"));
-
+console.log(Required.states());
+console.log(Required.senatorialDistricts("Plateau"));
+console.log(Required.lgas("oyo"));
